4c7aa1d666bb86d1ebb591822c642be248870e5a
Tomas Pilnaj <pilnaj.t@gmail.com>
2024-11-23 14:51:44 +0100

    enable SSG

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,15 @@
 import {useTranslations} from 'next-intl';
 import {Link} from '@/i18n/routing';
+import {setRequestLocale} from 'next-intl/server';
  
 export default function HomePage({
   params: {locale}
 }: {
   params: {locale: "en" | "cs"}
 }) {
+  // Enable static rendering
+  setRequestLocale(locale);
+
   const t = useTranslations('homepage');
   return (
     <div>
